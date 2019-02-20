1a5609cd7ba66753950e4ec1a98d18cc2c2c13a8
Anas BENSTITOU <anas.benstitou@neoxia.com>
2019-02-20 22:05:07 +0100

    Spring boot flyway integration

---
+++ src/main/resources/db/migration/V2__insert_data.sql
@@ -0,0 +1,2 @@
+INSERT INTO users(username, first_name, last_name) VALUES('anasbenstitou', 'Anas', 'BENSTITOU');
+INSERT INTO users(username, first_name, last_name) VALUES('testtest', 'Test', 'TEST');
